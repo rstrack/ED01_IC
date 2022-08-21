28e7b335a53a1f143a069221ec2ff6c367fb3619
Rafael Strack <strackinado@gmail.com>
2022-08-21 00:45:51 -0300

    inicial commit

---
+++ exercicio03_leaf.py
@@ -0,0 +1,27 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import time
+import pandas as p
+
+#LEAF
+
+df = p.read_csv(u'./leaf/leaf.csv')
+df_train, df_test, l_train, l_test = train_test_split(df.iloc[:, 1:16].values, df.iloc[:,0].values, test_size=0.2, random_state=int(time.time()))
+
+print("#################### DECISION TREE #########################")
+dt = DecisionTreeClassifier(class_weight='balanced', random_state=int(time.time()))
+dt.fit(df_train, l_train)
+dt_pred = dt.predict(df_test)
+accuracy = dt.score(df_test, l_test)
+print(f'Score: {accuracy}')
+print(classification_report(l_test, dt_pred))
+
+#NAIVE BAYES LEAF
+print("#################### NAIVE BAYES #########################")
+gnb = GaussianNB().fit(df_train, l_train)
+gnb_pred = gnb.predict(df_test)
+accuracy = gnb.score(df_test, l_test)
+print(f'Score: {accuracy}')
+print(classification_report(l_test, gnb_pred))
